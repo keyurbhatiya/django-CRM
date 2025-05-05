1e6103acd8b195a4f33c30f6f3cf88f1c5c34fad
keyurbhatiya <keyurbhatiya3004@gamil.com>
2025-05-05 15:54:31 +0530

    complete

--- website/views.py
+++ website/views.py
@@ -1,3 +1,99 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
-# Create your views here.
+
+def home(request):
+    records = Record.objects.all()
+
+
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in!"))
+            return render(request, 'home.html',{})
+        else:
+            messages.success(request, ("There was an error logging in, try again"))
+            return render(request, 'home.html',{})
+    return render(request, 'home.html',{'records':records})
+
+# def login_user(request):
+#     return render(request, 'login.html',{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out...!"))
+    return render(request, 'home.html',{})
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have successfully registered!"))
+            return render(request, 'home.html',{})
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html',{'form':form})
+    return render(request, 'register.html',{'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, ("You must be logged in to view that page..."))
+        return render(request, 'home.html',{})
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, ("Record deleted successfully..."))
+        return render(request, 'home.html',{})
+    else:
+        messages.success(request, ("You must be logged in to do that..."))
+        return render(request, 'home.html',{})
+    return render(request, 'home.html',{})  
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, ("Record Added..."))
+                return render(request, 'home.html',{})
+        return render(request, 'add_record.html',{'form':form})
+    else:
+        messages.success(request, ("You must be logged in to do that..."))
+        return render(request, 'home.html',{})
+    return render(request, 'add_record.html',{})
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Record Updated..."))
+            return render(request, 'home.html',{})
+        return render(request, 'update_record.html',{'form':form})
+    else:
+        messages.success(request, ("You must be logged in to do that..."))
+        return render(request, 'home.html',{})
+    return render(request, 'update_record.html',{})
+
